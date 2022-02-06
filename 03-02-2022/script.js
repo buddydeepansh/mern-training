a75d83504f8b34f7da83597cb4c6b074265b5024
DEEPANSH AGRAWAL <99.deepanshagrawal@gmail.com>
2022-02-06 12:27:40 +0530

    06-02-2022

--- 03-02-2022/script.js
+++ 03-02-2022/script.js
@@ -1,4 +1,5 @@
 // generate captcha
+var totalString, totalString2;
 function captchaGenerator() {
   var arr = ["a", "b", "c", "d", "A", "B", "C", "D"];
   var randomVariable1 = arr[Math.floor(Math.random() * arr.length)];
@@ -6,14 +7,14 @@ function captchaGenerator() {
   var randomVariable3 = arr[Math.floor(Math.random() * arr.length)];
   var randomVariable4 = arr[Math.floor(Math.random() * arr.length)];
   var randomVariable5 = arr[Math.floor(Math.random() * arr.length)];
-  var totalString =
+  totalString =
     randomVariable1 +
     randomVariable2 +
     randomVariable3 +
     randomVariable4 +
     randomVariable5;
   console.log(totalString);
-  var totalString2 =
+  totalString2 =
     randomVariable1 +
     " " +
     randomVariable2 +
@@ -24,10 +25,22 @@ function captchaGenerator() {
     " " +
     randomVariable5;
   console.log(totalString2);
-    var capp = document.getElementById('userinput').value;
-    var capp2 = document.querySelector('input[name="userinput"]');
-    console.log(capp,capp2);
-    capp.setAttribute("value", totalString2);
+
+  var capp = document.getElementById("userinput");
+  capp.setAttribute("value", totalString2);
 }
 captchaGenerator();
+
 // check captcha
+function checkCaptcha() {
+  var inputt = document.getElementById("inputCaptcha").value;
+  if (inputt === totalString) {
+    alert("Correct Captcha is Entered");
+    captchaGenerator();
+    document.getElementById("inputCaptcha").value = "";
+  } else {
+    alert("Incorrect Captcha is Entered");
+    captchaGenerator();
+    document.getElementById("inputCaptcha").value = "";
+  }
+}
